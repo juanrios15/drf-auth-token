66352ecbb1cc77afbe91dd17b819ea1963c0e1f1
Juan Rios <juankrios15@gmail.com>
2021-07-12 22:49:21 -0500

    added user_id to token auth

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from .serializers import CustomAuthTokenSerializer, RegisterUserSerializer
 
@@ -22,6 +24,15 @@ class RegisterUser(APIView):
 
 class CustomObtainAuthToken(ObtainAuthToken):
     serializer_class = CustomAuthTokenSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 
+                         'user_id': user.pk,
+                         'email': user.email})
 
 # Create your views here.
 class Logout(APIView):
